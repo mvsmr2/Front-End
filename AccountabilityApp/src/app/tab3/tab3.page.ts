824b051f64ca96478e9ee64211b407a408b85d07
Madeline Skora <mvsmr2@mst.edu>
2021-03-24 20:11:32 -0500

    Changed over from tab2 to tab3

--- AccountabilityApp/src/app/tab3/tab3.page.ts
+++ AccountabilityApp/src/app/tab3/tab3.page.ts
@@ -9,4 +9,22 @@ export class Tab3Page {
 
   constructor() {}
 
+  // IDK I ADDED THIS TO FIX DATETIME MAX AND MIN
+  public date: string = new Date().toISOString();
+  public time: any = new Date().getTime();
+  public currYear: string = new Date().toLocaleDateString();
+  public maxDate : any = (new Date()).getFullYear() + 2;
+  // public currHour: any = new Date().getHours() % 12 || 12
+  public currHour24: any = new Date().getHours() //still in 24 hr
+  public currAMPM: string = this.currHour24 >= 12? 'PM' : 'AM';
+  //make currHour in 12hr format
+  public currHour12: any = this.currHour24 % 12 || 12
+  // public currMinute: any = new Date().getMinutes();
+  // mins = ('0'+current_date.getMinutes()).slice(-2);
+  public currMinute: any = ('0' + (new Date().getMinutes())).slice(-2)
+  public currTime: any = this.currHour12 + ":" + this.currMinute + " " + this.currAMPM;
+
+  public buddies = ['Bud1', 'Bud2', 'Steve', 'Bud4']
+
+
 }
